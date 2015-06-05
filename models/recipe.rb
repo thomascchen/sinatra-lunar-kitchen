17d8f8888c164d6d2a343f6a5ebce32f46c3dbe8
Thomas Chen <chen.thomas.c@gmail.com>
2015-06-04 22:39:56 -0400

    Refactor
    * Remove ingredients as a parameter from Recipe class

--- models/recipe.rb
+++ models/recipe.rb
@@ -1,15 +1,37 @@
-require 'pry'
 require_relative 'ingredient.rb'
 
 class Recipe
-  attr_reader :id, :name, :ingredients, :instructions, :description
+  attr_reader :id, :name, :instructions, :description
 
-  def initialize(id, name, ingredients, instructions, description)
+  def initialize(id, name, instructions, description)
     @id = id
     @name = name
     @ingredients = ingredients
     @instructions = instructions
     @description = description
+  end
+
+  def db_connection
+    begin
+      connection = PG.connect(dbname: "recipes")
+      yield(connection)
+    ensure
+      connection.close
+    end
+  end
+
+  def ingredients
+    ingredients = db_connection do |conn|
+      conn.exec("SELECT name FROM ingredients WHERE recipe_id = $1", [id])
+    end
+
+    ingredients_array = []
+
+    ingredients.each do |ingredient|
+      ingredients_array << Ingredient.new(ingredient['name'])
+    end
+
+    ingredients_array
   end
 
   def self.db_connection
@@ -26,11 +48,7 @@ class Recipe
       conn.exec("SELECT * FROM recipes")
     end.to_a
 
-    ingredients = db_connection do |conn|
-      conn.exec("SELECT name FROM ingredients WHERE recipe_id = $1", [recipes[0]['id']]).to_a
-    end
-
-    recipes.map! { |recipe| Recipe.new(recipe["id"], recipe["name"], ingredients, recipe["instructions"], recipe["description"]) }
+    recipes.map! { |recipe| Recipe.new(recipe["id"], recipe["name"], recipe["instructions"], recipe["description"]) }
   end
 
   def self.find(id)
@@ -38,17 +56,7 @@ class Recipe
       conn.exec("SELECT * FROM recipes WHERE id = $1", [id])
     end.to_a
 
-    ingredients = db_connection do |conn|
-      conn.exec("SELECT name FROM ingredients WHERE recipe_id = $1", [recipe[0]['id']]).to_a
-    end
-
-    ingredients_array = []
-
-    ingredients.each do |ingredient|
-      ingredients_array << Ingredient.new(ingredient['name'])
-    end
-
-    Recipe.new(recipe[0]["id"].to_i, recipe[0]["name"], ingredients_array, recipe[0]["instructions"], recipe[0]["description"])
+    Recipe.new(recipe[0]["id"].to_i, recipe[0]["name"], recipe[0]["instructions"], recipe[0]["description"])
 
   end
 
